95a9d9ef40ee55e45f97e70232780d04863065f8
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-01-02 10:13:48 +0530

    added gallery

--- components/About/Demo.tsx
+++ components/About/Demo.tsx
@@ -3,41 +3,41 @@ import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 const images = [
     {
-        original: "https://placekitten.com/1000/600",
-        thumbnail: "https://placekitten.com/250/150",
+        original: "./images/slidergallery/1.png",
+        thumbnail: "./images/slidergallery/1.png",
       },
       {
-        original: "https://placekitten.com/1000/600",
-        thumbnail: "https://placekitten.com/250/150",
+        original: "./images/slidergallery/2.png",
+        thumbnail: "./images/slidergallery/2.png",
       },
       {
-        original: "https://placekitten.com/1000/600",
-        thumbnail: "https://placekitten.com/250/150",
+        original: "./images/slidergallery/3.png",
+        thumbnail: "./images/slidergallery/3.png",
       },
   {
-    original: "https://via.placeholder.com/1000x600",
-    thumbnail: "https://via.placeholder.com/250x150",
+    original: "/images/slidergallery/4.png",
+    thumbnail: "/images/slidergallery/4.png",
   },
  
   {
-    original: "https://placekitten.com/1000/600",
-    thumbnail: "https://placekitten.com/250/150",
+    original: "./images/slidergallery/5.png",
+    thumbnail: "./images/slidergallery/5.png",
   },
   {
-    original: "https://placekitten.com/1000/600",
-    thumbnail: "https://placekitten.com/250/150",
+    original: "./images/slidergallery/6.png",
+    thumbnail: "./images/slidergallery/6.png",
   },
   {
-    original: "https://placekitten.com/1000/600",
-    thumbnail: "https://placekitten.com/250/150",
+    original: "./images/slidergallery/1.png",
+    thumbnail: "./images/slidergallery/1.png",
   },
   {
-    original: "https://placekitten.com/1000/600",
-    thumbnail: "https://placekitten.com/250/150",
+    original: "./images/slidergallery/2.png",
+    thumbnail: "./images/slidergallery/2.png",
   },
   {
-    original: "https://placekitten.com/1000/600",
-    thumbnail: "https://placekitten.com/250/150",
+    original: "./images/slidergallery/3.png",
+    thumbnail: "./images/slidergallery/3.png",
   },
 ];
 
