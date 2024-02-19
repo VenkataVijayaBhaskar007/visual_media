17967daa7ddbef8376d578eca0992b8fc2220fc6
VenkataVijayaBhaskar007 <venkatavijayabhaskarvadada007@gmail.com>
2024-02-19 17:37:48 +0530

    tata

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -162,10 +162,10 @@ const Header = () => {
             </Link>
 
             <Link
-              href="/support"
+              href="tel:9346866605"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-blue-900 hover:translate-y-[-8px]"
             >
-              contact US 
+              Contact US 
             </Link>
           </div>
         </div>
